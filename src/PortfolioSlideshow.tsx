5c7facda1bfb773e42c6f817ae6d80560aff26ac
Piscionj <npisciotta16@gmail.com>
2022-03-12 01:15:33 -0500

    added bubble bobble slide

--- src/PortfolioSlideshow.tsx
+++ src/PortfolioSlideshow.tsx
@@ -11,12 +11,27 @@ const PortfolioSlideshow = () => {
               href="https://devpost.com/software/proteam-transportation-08"
               target="_blank"
             >
-              <img src="commuteindy.jpg" alt="First slide" />
+              <img height={500} src="commuteindy.jpg" alt="Commute indy app" />
             </a>
           </div>
           <Carousel.Caption>
             <h5>CommuteIndy</h5>
-            <p>An Android app that organizes local transportation options into one convenient place, allowing users to easily create a "community" through public transportation.</p>
+            <p>An Android app that organizes local transportation options into one convenient place, allowing users to easily create a "community" through public transportation</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div>
+            <a
+              href="https://javabubblebobble.web.app/"
+              target="_blank"
+            >
+              <img height={500} src="bubble-bobble.png" alt="Bubble bobble title screen" />
+            </a>
+          </div>
+          <Carousel.Caption>
+            <h5>Bubble Bobble</h5>
+            <p>A freshman year Java coding class project exported to the web using CheerpJ</p>
+            <p>Move with the arrow keys and shoot with the space bar</p>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
